f742f19350736f8ed1e0e7c0d7fd6e55d70b0f27
Rayyasa <rayya.a192018@gmail.com>
2023-03-10 14:10:05 +0700

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        ta: '#5268F4',
+      }
+    },
+  },
+  plugins: [],
+}
